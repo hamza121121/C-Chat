a454ddce5ad5e40eafd02fca7ffe78caa40dac5d
Hamza <hb65116@gmail.com>
2022-08-18 02:18:53 -0700

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Join from "./Component/Join/Join"
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Chat from './Component/Chat/Chat';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <div>
+      <Routes>
+         <Route path='/' element={<Join/>}/>
+         <Route path='/chat' element={<Chat/>}/>
+      </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
